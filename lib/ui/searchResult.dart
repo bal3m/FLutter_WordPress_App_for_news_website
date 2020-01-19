15cc64fbe3e30d552f40cbb41cefc84965903b33
BAL3M <j.balam8@gmail.com>
2020-01-19 13:33:05 +0200

    Search not found and ui design fix

--- lib/ui/searchResult.dart
+++ lib/ui/searchResult.dart
@@ -20,11 +20,54 @@ import 'home.dart';
 }
 
 class _ResultState extends State<Result> {
+   Widget BuildListView(BuildContext cntx, AsyncSnapshot snapshot){
+    return ListView.builder(
+       scrollDirection: Axis.vertical,
+       itemCount:find.length,
+       itemBuilder: (BuildContext cntx ,int index){
+         Map trend =snapshot.data[index];
+         String title= snapshot.data[index]['title']['rendered'];
+         String image= snapshot.data[index]['jetpack_featured_media_url'];
+         String content=snapshot.data[index]['content']['rendered'];
+         if(snapshot.data==[]){
+           return Center(
+             child: Text('لا توجد نتائج لعملية البحث',style: TextStyle(color: Colors.black),),
+           );
+         }else{
+           return InkWell(
+             onTap: ()=>{
+               Navigator.push(cntx, MaterialPageRoute(builder: (cntx)=> ViewPost(title: title,content: content,image: image,)))
+             },
+             child:  SlideItem(
+               title: parse(trend['title']['rendered'].toString()).documentElement.text,
+               img: trend['jetpack_featured_media_url']==''?imgurl:trend['jetpack_featured_media_url'],
+             ),
+           );
+         }
+
+       },
+     );
+
+   }
+   Widget SearchResault(BuildContext cntx){
+     return Center(
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[
+           Center(child: Icon(Icons.search,size: 100,)),
+
+             Text('لا توجد أي نتائج',style: TextStyle(fontSize: 25),),
+         ],
+       ),
+     );
+   }
   List find =new List();
   int currentIndex=2;
   String imgurl='http://w.almustaqbal.ly/wp-content/uploads/2019/06/64627701_442078543247525_3828041687351951360_n.png';
   Future <void> getLenght()async{
+    Future.delayed(const Duration(seconds: 5));
     find=await search(widget.search);
+    print(find.length);
   }
 
 //   FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
@@ -71,7 +114,7 @@ class _ResultState extends State<Result> {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text('  بحث',textDirection: TextDirection.rtl,)),backgroundColor: Color.fromRGBO(27,38,50,1),),
 
-      body: 
+      body:
       Padding(
         padding: const EdgeInsets.fromLTRB(10.0,0,10.0,0),
         child:Container(
@@ -80,26 +123,11 @@ class _ResultState extends State<Result> {
           child: FutureBuilder(
           future:search(widget.search) ,
           builder: (cntx,snapshot){
-        if(snapshot.hasData){
-          return ListView.builder(
-            scrollDirection: Axis.vertical,
-           itemCount:find.length,
-            itemBuilder: (BuildContext cntx ,int index){
-              Map trend =snapshot.data[index];
-              String title= snapshot.data[index]['title']['rendered'];
-              String image= snapshot.data[index]['jetpack_featured_media_url'];
-              String content=snapshot.data[index]['content']['rendered'];
-              return InkWell(
-                onTap: ()=>{
-                 Navigator.push(cntx, MaterialPageRoute(builder: (cntx)=> ViewPost(title: title,content: content,image: image,)))
-                },
-                child:  SlideItem(
-                title: parse(trend['title']['rendered'].toString()).documentElement.text,
-                img: trend['jetpack_featured_media_url']==''?imgurl:trend['jetpack_featured_media_url'],
-              ),
-              );
-            },
-          );
+        if(find.length!=0&&snapshot.hasData){
+          print(snapshot.hasData);
+          return BuildListView(cntx, snapshot);
+        }else if(snapshot.hasData&&find.length==0){
+          return SearchResault(cntx);
         }
         return Center(child: CircularProgressIndicator(
           backgroundColor: Color.fromRGBO(212, 175, 55, 1),
@@ -116,29 +144,29 @@ class _ResultState extends State<Result> {
         itemCornerRadius: 8,
         onItemSelected: (index) => setState(() {
           currentIndex = index;
-          switch(index) { 
-              case 0: { 
+          switch(index) {
+              case 0: {
                 Navigator.push(context, MaterialPageRoute(builder: (context)=>Whome()));
-              } 
-            break; 
-  
+              }
+            break;
+
            case 1: {
               Navigator.push(context, MaterialPageRoute(builder: (context)=>TabBarDemo()));
-            } 
-            break; 
-            
+            }
+            break;
+
              case 2: {
               Navigator.push(context, MaterialPageRoute(builder: (context)=>Search()));
-            } 
-            break; 
+            }
+            break;
             case 3: {
               Navigator.push(context, MaterialPageRoute(builder: (context)=>Home()));
-            } 
+            }
             break;
-             default: { 
-      //statements;  
+             default: {
+      //statements;
              }
-           break; 
+           break;
                }
         }),
         items: [
